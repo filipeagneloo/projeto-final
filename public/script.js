77e571ce8d24ddb0fde6c498159d61bfe6329e91
filipeagneloo <filipe.agnelo@uni9.edu.br>
2025-05-29 01:44:32 +0000

    Altera fetch do frontend para rota /inserir

--- public/script.js
+++ public/script.js
@@ -11,10 +11,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
       const dados = { nome_completo, email, celular, opcao_escolhida };
 
-      const resposta = await fetch('https://projeto-final-khaki-five.vercel.app/api/form', {
-         method: 'POST',
-         headers: { 'Content-Type': 'application/json' },
-         body: JSON.stringify(dados)
+      const resposta = await fetch('/inserir', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(dados)
       });
 
       const resultado = await resposta.json();
